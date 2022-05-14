fa4247d767f4dd95b26e6ce452cb4ce77714412e
faysalhadi <faisalhadi181@gmail.com>
2022-05-14 11:09:04 -0800

    commit

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -16,34 +16,36 @@ class DashboardScreen extends StatelessWidget {
         padding: EdgeInsets.all(defaultPadding),
         child: Column(
           children: [
-            Header(),
-            SizedBox(height: defaultPadding),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  flex: 5,
-                  child: Column(
-                    children: [
-                      MyFiles(),
-                      SizedBox(height: defaultPadding),
-                      RecentFiles(),
-                      if (Responsive.isMobile(context))
-                        SizedBox(height: defaultPadding),
-                      if (Responsive.isMobile(context)) StarageDetails(),
-                    ],
-                  ),
-                ),
-                if (!Responsive.isMobile(context))
-                  SizedBox(width: defaultPadding),
-                // On Mobile means if the screen is less than 850 we dont want to show it
-                if (!Responsive.isMobile(context))
-                  Expanded(
-                    flex: 2,
-                    child: StarageDetails(),
-                  ),
-              ],
-            )
+            SizedBox(height: 50,),
+            RecentFiles()
+            //Header(),
+            //SizedBox(height: defaultPadding),
+            // Row(
+            //   crossAxisAlignment: CrossAxisAlignment.start,
+            //   children: [
+            //     Expanded(
+            //       flex: 5,
+            //       child: Column(
+            //         children: [
+            //           MyFiles(),
+            //           SizedBox(height: defaultPadding),
+            //           RecentFiles(),
+            //           if (Responsive.isMobile(context))
+            //             SizedBox(height: defaultPadding),
+            //           if (Responsive.isMobile(context)) StarageDetails(),
+            //         ],
+            //       ),
+            //     ),
+            //     if (!Responsive.isMobile(context))
+            //       SizedBox(width: defaultPadding),
+            //     // On Mobile means if the screen is less than 850 we dont want to show it
+            //     if (!Responsive.isMobile(context))
+            //       Expanded(
+            //         flex: 2,
+            //         child: StarageDetails(),
+            //       ),
+            //   ],
+            // )
           ],
         ),
       ),
